5891dbc3334c326447e0f5e08703671930138397
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2024-08-19 20:30:11 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# SilgiUb-nstaller
+<div align="center">
+  <img src="https://telegra.ph/file/2269e1ed5b9a3b0444361.jpg" width="200" height="200">
+  <h1>⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝ Qurulum</h1>
+</div>
+
+## HerokuAPI'si işlədilərək edilmiş Avtomatik ⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝ Deploy
+
+## [⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝ Repo](https://github.com/Silgimusicbot/SilgiUserbot)
+
+## Qurulum
+```sh
+git clone https://github.com/Silgimusicbot/SilgiUbİnstaller
+cd SilgiUbİnstaller
+pip install -r requirements.txt
+python3 -m silgi_installer
+```
+
+## Qurucu
+
+[SILGI](https://t.me/atondusalamde)
+
+## Lisenziya
+Bu proyekt GPL-3.0 lisenziyası ilə qorunur.
---
+++ silgi.sh
@@ -0,0 +1,48 @@
+Userator+="\n? ????? ??????? ?? - AVTOMATIK QURULUM �?"
+MESAJ+="\n "
+MESAJ+="\n✅Yeniliklər kanalımız: @silgiubplugin"
+MESAJ+="\n✅Dəstək qrupumuz: @silgiuserbot"
+MESAJ+="\n "
+KOMEK+="\n "
+BOSLUQ="\n "
+clear
+echo -e $BOSLUQ
+echo -e $BOSLUQ
+pkg update -y
+clear
+echo -e $BOSLUQ
+apt upgrade -y
+echo -e $BOSLUQ
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo "Python yüklənir..."
+echo -e $BOSLUK
+pkg install python -y
+pip install --upgrade pip
+clear
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo "Git yüklənir..."
+echo -e $BOSLUQ
+pkg install git -y
+clear
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo "Telethon yüklənir..."
+echo -e $BOSLUQ
+pip install telethon
+clear
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo "Faylları yükləyirəm..."
+echo -e $BOSLUQ
+rm -rf SilgiUb?nstaller
+git clone https://github.com/Silgimusicbot/SilgiUserbot
+clear
+echo -e $BOSLUK
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo -e $BOSLUQ
+cd SilgiUb?nstaller
+pip install -r requirements.txt
+python -m silgi_installer
