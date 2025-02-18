45e55e81a0d12810814a383a63214577f93a2367
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-02-18 23:34:09 +0400

    Update silgi.sh

--- silgi.sh
+++ silgi.sh
@@ -1,4 +1,4 @@
-Userator+="\n ⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝💻- AVTOMATIK QURULUM �?"
+SilgiUserbot+="\n ⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝💻- AVTOMATIK QURULUM �?"
 MESAJ+="\n "
 MESAJ+="\n✅Yeniliklər kanalımız: @silgiubplugin"
 MESAJ+="\n✅Dəstək qrupumuz: @silgiuserbot"
